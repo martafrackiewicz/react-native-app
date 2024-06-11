0126c4966913dc7211e6ce9487973bbee4727da0
Marta Frąckiewicz <mfrackiewicz@animativ.pl>
2024-06-11 14:09:04 +0200

    testowa strona z linkiem

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: 'lightblue',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/test-link" style={{
+        backgroundColor: 'white', 
+        borderStyle: 'solid', 
+        borderColor: 'solid', 
+        borderWidth: 1, 
+        paddingVertical: 4, 
+        paddingHorizontal: 8, 
+        alignSelf: 'auto', 
+        width: '30%', 
+        textAlign: 'center', 
+        margin: 10}}>
+          Test link
+      </Link>
+      <Text style={{alignSelf: 'center'}}>tu bedzie todo lista</Text>
     </View>
   );
 }
---
+++ app/test-link.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: 'rebeccapurple',
+      }}
+    >
+      <Text style={{color: 'white'}}>To jest test link</Text>
+    </View>
+  );
+}
