a39fd23663fa2da82d2149ed63607d412ac36d59
Marta Frąckiewicz <mfrackiewicz@animativ.pl>
2024-06-12 17:16:43 +0200

    to do list screen with button and modal

--- app/(tabs)/todo.tsx
+++ app/(tabs)/todo.tsx
@@ -1,13 +1,75 @@
-import { Text, View } from 'react-native';
+import CustomButton from '@/components/CustomButton';
+import { SafeAreaView, FlatList, Text, View, Modal, Button, Pressable } from 'react-native';
+import { useState } from 'react';
+
+const data = [
+  {
+    id: '1',
+    title: 'First Item',
+  },
+  {
+    id: '2',
+    title: 'Second Item',
+  },
+  {
+    id: '3',
+    title: 'Third Item',
+  },
+];
+
+type ItemProps = { title: string };
+const Item = ({ title }: ItemProps) => (
+  <View className="p-5">
+    <Text className="font-lato text-2xl">{`\u003e ${title}`}</Text>
+  </View>
+);
 
 export default function ToDo() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handlePress = () => {
+    // alert('press');
+    setModalVisible(true);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'lightblue',
-      }}>
-      <Text className="text-xl font-spacemono">ToDo</Text>
-    </View>
+    <SafeAreaView>
+      <View className="p-5 h-full" style={{ backgroundColor: 'lightblue' }}>
+        <Text className="font-lato-bold text-2xl">ToDo List</Text>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Item title={item.title} />}
+          keyExtractor={(item) => item.id}
+          className="mt-10"
+        />
+        <CustomButton
+          handlePress={handlePress}
+          title={`\u002B Add`}
+          containerStyle=""></CustomButton>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            }}>
+            <View style={{ backgroundColor: 'white', padding: 20 }}>
+              <Text>This is the modal content</Text>
+              <Button title="Submit" onPress={() => setModalVisible(!modalVisible)} />
+              {/* <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                <Text>Hide Modal</Text>
+              </Pressable> */}
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </SafeAreaView>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,22 @@
 import { ScrollView, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Image } from 'expo-image';
-import { router } from 'expo-router';
-import Button from '@/components/Button';
+import { Link, router } from 'expo-router';
+import CustomButton from '@/components/CustomButton';
 
 export default function Index() {
   return (
-    <SafeAreaView className="bg-sky-100 h-full border-2 border-blue-500 border-solid">
+    <SafeAreaView
+      className="bg-sky-100 h-full"
+      // className="bg-sky-100 h-full border-2 border-blue-500 border-solid"
+    >
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full flex items-center justify-center px-5 border-2 border-green-500 border-solid">
+        <View
+          className="w-full flex items-center justify-center px-5"
+          // className="w-full flex items-center justify-center px-5 border-2 border-green-500 border-solid"
+        >
           <Image
-            source={require('../assets/images/icon.png')}
+            source={require('../assets/images/icon.webp')}
             // source="https://picsum.photos/seed/696/3000/2000"
             contentFit="contain"
             transition={1000}
@@ -23,12 +29,17 @@ export default function Index() {
           <Text className="font-lato text-xl text-center mt-4 text-gray-800">
             Capture moments, organize tasks, and embrace life’s journey with our app
           </Text>
-          <Button
+          <CustomButton
             title="Continue"
             handlePress={() => {
               router.push('/todo');
             }}
-            containerStyle="mt-8"></Button>
+            containerStyle="mt-8"></CustomButton>
+        </View>
+        <View className="px-5 flex-1">
+          <Link href="/test-link" className="self-end p-2 bg-white font-lato-bold mt-auto mb-20">
+            Test link
+          </Link>
         </View>
       </ScrollView>
     </SafeAreaView>
