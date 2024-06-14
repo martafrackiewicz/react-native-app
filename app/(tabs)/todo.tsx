e3d04b15ef631534a7dadc600b476cf2db2b376a
Marta Frąckiewicz <mfrackiewicz@animativ.pl>
2024-06-14 13:37:28 +0200

    tasks - add new task functionality

--- app/(tabs)/todo.tsx
+++ app/(tabs)/todo.tsx
@@ -1,35 +1,70 @@
 import CustomButton from '@/components/CustomButton';
-import { SafeAreaView, FlatList, Text, View, Modal, Button, Pressable } from 'react-native';
-import { useState } from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  View,
+  Modal,
+  Button,
+  Pressable,
+  TextInput,
+} from 'react-native';
+import { SetStateAction, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
 
-const data = [
-  {
-    id: '1',
-    title: 'First Item',
-  },
-  {
-    id: '2',
-    title: 'Second Item',
-  },
-  {
-    id: '3',
-    title: 'Third Item',
-  },
-];
+type FormData = {
+  title: string;
+  priority: string;
+  // priority: boolean;
+};
+type ItemProps = { title: string; priority: string };
+// priority: boolean
 
-type ItemProps = { title: string };
-const Item = ({ title }: ItemProps) => (
-  <View className="p-5">
+const Item = ({ title, priority }: ItemProps) => (
+  <View className="flex flex-row items-end p-5">
     <Text className="font-lato text-2xl">{`\u003e ${title}`}</Text>
+    <Text className="font-lato text-lg ml-auto">{priority}</Text>
   </View>
 );
 
 export default function ToDo() {
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'First Item',
+      priority: 'low',
+      // priority: true,
+    },
+    {
+      id: '2',
+      title: 'Second Item',
+      priority: 'high',
+      // priority: false,
+    },
+  ]);
   const [modalVisible, setModalVisible] = useState(false);
 
   const handlePress = () => {
-    // alert('press');
     setModalVisible(true);
+  };
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({});
+
+  const onSubmit = (data: FormData) => {
+    alert(JSON.stringify(data));
+    console.log(data);
+
+    setModalVisible(!modalVisible);
+    reset();
+
+    const id = (tasks.length + 1).toString();
+
+    setTasks([...tasks, { ...data, id }]);
   };
 
   return (
@@ -37,8 +72,8 @@ export default function ToDo() {
       <View className="p-5 h-full" style={{ backgroundColor: 'lightblue' }}>
         <Text className="font-lato-bold text-2xl">ToDo List</Text>
         <FlatList
-          data={data}
-          renderItem={({ item }) => <Item title={item.title} />}
+          data={tasks}
+          renderItem={({ item }) => <Item title={item.title} priority={item.priority} />}
           keyExtractor={(item) => item.id}
           className="mt-10"
         />
@@ -60,9 +95,50 @@ export default function ToDo() {
               alignItems: 'center',
               backgroundColor: 'rgba(0, 0, 0, 0.5)',
             }}>
-            <View style={{ backgroundColor: 'white', padding: 20 }}>
-              <Text>This is the modal content</Text>
-              <Button title="Submit" onPress={() => setModalVisible(!modalVisible)} />
+            <View style={{ backgroundColor: 'white', padding: 20, minWidth: '90%' }}>
+              <View>
+                <Text>Title*</Text>
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      onBlur={onBlur}
+                      onChangeText={(value) => onChange(value)}
+                      value={value}
+                      className="border border-gray-300 p-2 rounded-md focus:border-blue-500 focus:ring-2 focus:ring-blue-200 mt-2"
+                    />
+                  )}
+                  name="title"
+                  rules={{ required: true }}
+                  defaultValue=""
+                />
+                {errors.title?.type === 'required' && (
+                  <Text className="font-lato text-red-500">Title is required</Text>
+                )}
+                <Text className="mt-4">Priority*</Text>
+                <Controller
+                  control={control}
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      onBlur={onBlur}
+                      onChangeText={(value) => onChange(value)}
+                      value={value}
+                      className="border border-gray-300 p-2 rounded-md focus:border-blue-500 focus:ring-2 focus:ring-blue-200 mt-2"
+                    />
+                  )}
+                  name="priority"
+                  rules={{ required: true }}
+                  defaultValue=""
+                  // defaultValue={false}
+                />
+                {errors.priority?.type === 'required' && (
+                  <Text className="font-lato text-red-500">Priority is required</Text>
+                )}
+                <View>
+                  <Button color="black" title="Add" onPress={handleSubmit(onSubmit)} />
+                </View>
+              </View>
+              {/* <Button title="Submit" onPress={() => setModalVisible(!modalVisible)} /> */}
               {/* <Pressable onPress={() => setModalVisible(!modalVisible)}>
                 <Text>Hide Modal</Text>
               </Pressable> */}
